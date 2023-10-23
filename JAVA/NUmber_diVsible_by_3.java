c288e19d8dd3fcee3b703a90455c4454a48c4a50
SUVAM DAS <139239891+vamus092@users.noreply.github.com>
2023-10-23 23:53:44 +0530

    Create NUmber_diVsible_by_3.java

---
+++ JAVA/NUmber_diVsible_by_3.java
@@ -0,0 +1,13 @@
+class Solution {
+    static int isPossible(int N, int arr[]) {
+        // code here
+        long sum=0;
+        for(int i=0;i<N;i++){
+            sum+=arr[i];
+        }
+        if(sum%3==0)
+        return 1;
+        return 0;
+    }
+}
+ 
